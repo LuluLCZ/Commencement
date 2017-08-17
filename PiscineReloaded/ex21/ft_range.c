f5dbf2712d55db45d36ee57cc6eca0e008c9cf53
LuluLCZ <lacaze.lulu@gmail.com>
2017-08-17 22:35:57 +0200

    ok

--- PiscineReloaded/ex21/ft_range.c
+++ PiscineReloaded/ex21/ft_range.c
@@ -1,50 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Lulu <Lulu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/14 12:48:10 by Lulu              #+#    #+#             */
-/*   Updated: 2017/08/15 16:11:00 by Lulu             ###   ########.fr       */
+/*   Created: 2017/08/15 16:11:34 by Lulu              #+#    #+#             */
+/*   Updated: 2017/08/15 19:58:35 by Lulu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
-int     ft_strlen(char *src)
+int     *ft_range(int min, int max)
 {
+    int     *tab;
     int     i;
 
     i = 0;
-    while (src[i])
-        i++;
-    return (i);
-}
-
-char    *ft_strdup(char *src)
-{
-    char    *dest;
-    int     i;
-
-    i= 0;
-    if(!(dest = (char *)malloc(sizeof(char) * ft_strlen(src) + 1)))
-        return (NULL);
-    while (src[i] != '\0')
+    if (min >= max)
+        return (0);
+    if (!(tab = (int *)malloc(sizeof(int) * (max + min) + 1)))
+        return (0);
+    while (min <= max)
     {
-        dest[i] = src[i];
+        tab[i] = min;
         i++;
+        min++;
     }
-    dest[i] = '\0';
-    return (dest);
+    tab[i] = '\0';
+    return (tab);
 }
 
 int     main(int ac, char **av)
 {
-    if (ac != 2)
-        return (0);
-    printf("%s\n", ft_strdup(av[1]));
-    //printf("%s", strdup(av[1]));
+    int     *tab;
+    int     i;
+    (void)ac;
+    (void)av;
+
+    tab = ft_range(0, 10);
+    i = 0;
+    while (i < 11)
+    {
+        printf("%d", tab[i]);
+        i++;
+    }
+    return (0);
 }
